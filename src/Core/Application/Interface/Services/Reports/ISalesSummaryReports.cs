065b51362314a6a239ec86f30f396f3d9b1a7574
Charlito7 <59016855+Charlito7@users.noreply.github.com>
2025-06-24 23:12:55 -0400

    Reports

---
+++ src/Core/Application/Interface/Services/Reports/ISalesSummaryReports.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Interface.Services.Reports;
+
+public interface ISalesSummaryReports
+{
+    Task DailySalesSummaryReports();
+}
